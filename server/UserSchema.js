cbf86cb955c503325b5a654eb26988644327ec7c
ZachBenedicto <142936664+ZachBenedicto@users.noreply.github.com>
2025-05-05 17:48:20 -0400

    Update UserSchema.js

--- server/UserSchema.js
+++ server/UserSchema.js
@@ -4,15 +4,7 @@ const UserSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
     username: String,
-    password: String,
-    preferences: {
-        cuisines:   { type: [String], default: [] },
-        priceRange: {
-            min: { type: Number, default: 1 },
-            max: { type: Number, default: 5 }
-        },
-        distance:   { type: Number, default: 5 }
-    }
+    password: String   
 });
 
 const User = mongoose.model("User", UserSchema);
