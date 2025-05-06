3a4f251d2e729cd83e1f24f7cc399b7c10789499
ZachBenedicto <142936664+ZachBenedicto@users.noreply.github.com>
2025-05-06 14:08:24 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,30 @@
+import React from 'react';
 import './App.css';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Login from './Login';
-import SignUp from './SignUp';
-import RestSignUp from './RestSignUp';
-import HomeScreen from './Homepage';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Header from './Header';
+import Login       from './Login';
+import SignUp      from './SignUp';
+import RestSignUp  from './RestSignUp';
+import Preferences from './Preferences';
+import Homepage    from './Homepage';
+import Favorites   from './Favorites';
 
-function App(){
-    return(
-        <BrowserRouter>
-        <Routes>
-            <Route path = "/" element = {<Login/>} />
-            <Route path = "Login" element = {<Login/>}/>
-            <Route path = "SignUp" element = {<SignUp/>}/>
-            <Route path = "RestSignUp" element = {<RestSignUp/>}/>
-            <Route path = "HomeScreen" element = {<HomeScreen/>}/>
-        </Routes>
-        </BrowserRouter>
-    );
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/"            element={<Login />}      />
+        <Route path="/login"       element={<Login />}      />
+        <Route path="/signup"      element={<SignUp />}     />
+        <Route path="/restsignup"  element={<RestSignUp />} />
+        <Route path="/preferences" element={<Preferences/>} />
+        <Route path="/homepage"    element={<Homepage />}   />
+        <Route path="/favorites"   element={<Favorites />}  />
+        <Route path="*"            element={<Navigate to="/" replace/>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
