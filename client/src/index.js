abe9bbe138856cc85042607dd10bcbce30380326
ZachBenedicto <142936664+ZachBenedicto@users.noreply.github.com>
2025-05-06 14:09:12 -0400

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,38 +1,13 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
-import Login from './Login';
-import SignUp from './SignUp';
-import RestSignUp from './RestSignUp';
-import HomeScreen from './Homepage';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-    <Route path = "/" element = {<Login/>}/>
-    <Route path = "/Login" element = {<Login/>}/>
-    <Route path = "/SignUp" element = {<SignUp/>}/>
-    <Route path = "/RestSignUp" element = {<RestSignUp/>}/>
-    <Route path = "HomeScreen" element = {<HomeScreen/>}/>
-    </>
-  )
-)
-
 root.render(
   <React.StrictMode>
-    <RouterProvider router = {router}>
     <App />
-    </RouterProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
